9520196b40191b9f460e6a2c9b26ed2b561c0775
Milos Vasic <milos.vasic@harness.io>
2021-10-04 14:12:52 +0200

    FFM-1179 - Small cleanup.

--- src/main/java/io/harness/cf/client/api/AuthService.java
+++ src/main/java/io/harness/cf/client/api/AuthService.java
@@ -6,62 +6,74 @@ import com.google.common.util.concurrent.AbstractScheduledService;
 import io.harness.cf.ApiException;
 import io.harness.cf.api.DefaultApi;
 import io.harness.cf.client.dto.AuthenticationRequestBuilder;
+import io.harness.cf.model.AuthenticationRequest;
 import io.harness.cf.model.AuthenticationResponse;
+
 import java.util.concurrent.TimeUnit;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class AuthService extends AbstractScheduledService {
 
-  protected final String apiKey;
-  protected final CfClient cfClient;
-  protected final DefaultApi defaultApi;
-  protected final int pollIntervalInSec;
+    protected final String apiKey;
+    protected final CfClient cfClient;
+    protected final DefaultApi defaultApi;
+    protected final int pollIntervalInSec;
 
-  public AuthService(
-      DefaultApi defaultApi, String apiKey, CfClient cfClient, int pollIntervalInSec) {
+    public AuthService(
 
-    this.defaultApi = defaultApi;
-    this.apiKey = apiKey;
-    this.cfClient = cfClient;
-    this.pollIntervalInSec = pollIntervalInSec;
-  }
+            final DefaultApi defaultApi,
+            final String apiKey,
+            final CfClient cfClient,
+            final int pollIntervalInSec
+    ) {
 
-  @Override
-  protected void runOneIteration() throws Exception {
-
-    if (isNullOrEmpty(apiKey)) {
-
-      throw new CfClientException("SDK key cannot be empty");
+        this.defaultApi = defaultApi;
+        this.apiKey = apiKey;
+        this.cfClient = cfClient;
+        this.pollIntervalInSec = pollIntervalInSec;
     }
 
-    try {
+    @Override
+    protected void runOneIteration() throws Exception {
 
-      AuthenticationResponse authResponse =
-          defaultApi.authenticate(
-              AuthenticationRequestBuilder.anAuthenticationRequest().apiKey(apiKey).build());
+        if (isNullOrEmpty(apiKey)) {
 
-      String jwtToken = authResponse.getAuthToken();
-      cfClient.setJwtToken(jwtToken);
-      cfClient.init();
-      log.info("Stopping Auth service");
-      this.stopAsync();
+            throw new CfClientException("SDK key cannot be empty");
+        }
 
-    } catch (ApiException apiException) {
+        try {
 
-      log.error("Failed to get auth token {}", apiException.getMessage());
-      if (apiException.getCode() == 401 || apiException.getCode() == 403) {
+            final AuthenticationRequest request = AuthenticationRequestBuilder
+                    .anAuthenticationRequest()
+                    .apiKey(apiKey)
+                    .build();
 
-        String errorMsg = String.format("Invalid apiKey %s. Serving default value. ", apiKey);
-        log.error(errorMsg);
-        throw new CfClientException(errorMsg);
-      }
+            final AuthenticationResponse authResponse = defaultApi.authenticate(request);
+
+            String jwtToken = authResponse.getAuthToken();
+            cfClient.setJwtToken(jwtToken);
+            cfClient.init();
+            log.info("Stopping Auth service");
+
+            this.stopAsync();
+
+        } catch (ApiException apiException) {
+
+            log.error("Failed to get auth token {}", apiException.getMessage());
+            if (apiException.getCode() == 401 || apiException.getCode() == 403) {
+
+                String errorMsg = String.format("Invalid apiKey %s. Serving default value. ", apiKey);
+                log.error(errorMsg);
+                throw new CfClientException(errorMsg);
+            }
+        }
     }
-  }
 
-  @Override
-  protected Scheduler scheduler() {
+    @Override
+    protected Scheduler scheduler() {
 
-    return Scheduler.newFixedDelaySchedule(0L, pollIntervalInSec, TimeUnit.SECONDS);
-  }
+        return Scheduler.newFixedDelaySchedule(0L, pollIntervalInSec, TimeUnit.SECONDS);
+    }
 }
